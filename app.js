0e55d72f1996b5a98ed97032499fb07a2642ee72
Mohammad Aslam <Taiyab.aslam728@gmail.com>
2023-01-04 17:02:51 -0600

    added js

--- app.js
+++ app.js
@@ -9,3 +9,10 @@
 /* Display Functions */
 
 // (don't forget to call any display functions you want to run on page load!)
+const displayName = document.getElementById('nameDisplay');
+const displayBtn = document.getElementById('submit');
+const nameInput = document.getElementById('name-input');
+
+console.log(displayName);
+console.log(displayBtn);
+console.log(nameInput);
